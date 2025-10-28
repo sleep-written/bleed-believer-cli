312a106624495bac5e7db55fa94f33fa8a5c22c2
sleep-written <sleep.written0000@gmail.com>
2025-10-28 17:56:57 -0300

    Fixed resolve custom hook unit tests.

--- package/src/lib/resolve-custom-hook/resolve-custom-hook.test.ts
+++ package/src/lib/resolve-custom-hook/resolve-custom-hook.test.ts
@@ -2,8 +2,8 @@ import type { ResolveCustomHookInject, DefaultResolve } from './interfaces/index
 import type { ResolveHookContext } from 'module';
 import type { TSConfig } from '@lib/ts-config/index.js';
 
-import { pathToFileURL } from 'url';
 import { ResolveCustomHook } from './resolve-custom-hook.js';
+import { pathToFileURL } from 'url';
 import test from 'ava';
 
 const inject: ResolveCustomHookInject = {
@@ -18,7 +18,7 @@ const inject: ResolveCustomHookInject = {
             }
 
             default: {
-                throw new Error(`The file "${path}" doesn't exists`);
+                throw new Error(`File isn\'t accesible`);
             }
         }
     }
@@ -26,13 +26,22 @@ const inject: ResolveCustomHookInject = {
 
 const defaultResolve: DefaultResolve = (specifier) => {
     switch (specifier) {
-        case'@tool/daemon/index.js':
-        case'@tool/daemon/index.js': {
-            throw new Error('Not found!');
+        case 'crypto': {
+            return {
+                url: `node:${specifier}`,
+                format: 'builtin-module'
+            };
+        }
+
+        case '/path/to/project/dist/tool/daemon/index.js':
+        case '/path/to/project/src/tool/daemon/index.ts': {
+            return {
+                url: specifier
+            }
         }
 
         default: {
-            return { url: specifier };
+            throw new Error('Cannot resolve url');
         }
     }
 };
@@ -49,6 +58,18 @@ const tsConfig: TSConfig = {
     }
 };
 
+test('"crypto" → "crypto" (builtin)', async t => {
+    const context: ResolveHookContext = {
+        parentURL: pathToFileURL('/path/to/project/src/tool/satan/satan.ts').href,
+        conditions: [],
+        importAttributes: {}
+    };
+
+    const matcher = new ResolveCustomHook(tsConfig, inject);
+    const result = await matcher.resolve('crypto', context, defaultResolve);
+    t.is(result.url, 'node:crypto');
+});
+
 test('"@tool/daemon/index.js" → "/path/to/project/src/tool/daemon/index.ts"', async t => {
     const context: ResolveHookContext = {
         parentURL: pathToFileURL('/path/to/project/src/tool/satan/satan.ts').href,
@@ -61,6 +82,20 @@ test('"@tool/daemon/index.js" → "/path/to/project/src/tool/daemon/index.ts"', 
     t.is(result.url, '/path/to/project/src/tool/daemon/index.ts');
 });
 
+test('"@tool/demigod/index.js" → "/path/to/project/src/tool/daemon/index.ts" (not found)', async t => {
+    const context: ResolveHookContext = {
+        parentURL: pathToFileURL('/path/to/project/src/tool/satan/satan.ts').href,
+        conditions: [],
+        importAttributes: {}
+    };
+
+    const matcher = new ResolveCustomHook(tsConfig, inject);
+    await t.throwsAsync(
+        () => matcher.resolve('@tool/demigod/index.js', context, defaultResolve),
+        { message: `Cannot resolve url` }
+    );
+});
+
 test('"@tool/daemon/index.js" → "/path/to/project/dist/tool/daemon/index.js"', async t => {
     const context: ResolveHookContext = {
         parentURL: pathToFileURL('/path/to/project/dist/tool/satan/satan.js').href,
@@ -71,4 +106,18 @@ test('"@tool/daemon/index.js" → "/path/to/project/dist/tool/daemon/index.js"',
     const matcher = new ResolveCustomHook(tsConfig, inject);
     const result = await matcher.resolve('@tool/daemon/index.js', context, defaultResolve);
     t.is(result.url, '/path/to/project/dist/tool/daemon/index.js');
+});
+
+test('"@tool/demigod/index.js" → "/path/to/project/dist/tool/daemon/index.js" (not found)', async t => {
+    const context: ResolveHookContext = {
+        parentURL: pathToFileURL('/path/to/project/dist/tool/satan/satan.js').href,
+        conditions: [],
+        importAttributes: {}
+    };
+
+    const matcher = new ResolveCustomHook(tsConfig, inject);
+    await t.throwsAsync(
+        () => matcher.resolve('@tool/demigod/index.js', context, defaultResolve),
+        { message: `Cannot resolve url` }
+    );
 });
