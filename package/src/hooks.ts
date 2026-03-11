357ffe3a8473cd8b2f1580529228404b080a41d2
sleep-written <sleep.written0000@gmail.com>
2026-03-11 12:37:48 -0300

    Added a better hook display to test.

--- package/src/hooks.ts
+++ package/src/hooks.ts
@@ -1,17 +1,27 @@
 import type { LoadHook, ResolveHook } from 'node:module';
 import { AliasResolver } from './lib/alias-resolver/index.ts';
 import { Tsconfig } from './lib/tsconfig/index.ts';
+import { styleText } from 'node:util';
 
 const tsconfig = await Tsconfig.load('tsconfig.json');
 const resolver = new AliasResolver(tsconfig);
 
 export const load: LoadHook = (url, context, defaultLoad) => {
-    console.log(`load hook:\nurl: ${url}\ncontext: ${context}`);
+    console.log(styleText('yellow', 'Load hook') + ':');
+    console.log('----------');
+    console.log('url:', url);
+    console.log('context:', context);
+    console.log('\n');
+
     return defaultLoad(url, context);
 }
 
 export const resolve: ResolveHook = (specifier, context, defaultResolve) => {
-    console.log(`resolve hook:\nspecifier: ${specifier}\ncontext: ${context}`);
+    console.log(styleText('yellow', 'Resolve hook') + ':');
+    console.log('-------------');
+    console.log('specifier:', specifier);
+    console.log('context:', context);
+    console.log('\n');
 
     const resolvedSpecifier = resolver.resolve(specifier)?.[0] ?? specifier;
     return defaultResolve(resolvedSpecifier, context);
